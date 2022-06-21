a813900c7e308d405f46de21671390937aff7384
mdfarhaan <farhaanm110@gmail.com>
2022-06-21 23:39:40 +0530

    Initial Commit

---
+++ primeFactor.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+void primeFactors(int n)
+{
+    while (n % 2 == 0) {
+        printf("%d ", 2);
+        n = n / 2;
+    }
+    for (int i = 3; i <= sqrt(n); i = i + 2) {
+        while (n % i == 0) {
+            printf("%d ", i);
+            n = n / i;
+        }
+    }
+    if (n > 2)
+        printf("%d ", n);
+}
+
+int main()
+{
+    int n = 315;
+    primeFactors(n);
+    return 0;
+}
---
+++ sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void sort(int n, int* ptr)
+{
+    int i, j, t;
+    for (i = 0; i < n; i++) {
+    for (j = i + 1; j < n; j++) {
+
+    if (*(ptr + j) < *(ptr + i)) {
+    t = *(ptr + i);
+    *(ptr + i) = *(ptr + j);
+    *(ptr + j) = t;
+    }
+    }
+    }
+    for (i = 0; i < n; i++)
+    printf("%d ", *(ptr + i));
+}
+
+int main()
+{
+    int n = 7;
+    int arr[] = { 0, 20, 17, 32, 7, 19, 77 };
+    sort(n, arr);
+    return 0;
+}
